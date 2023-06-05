3a90034f1db5635077a44b09154ffe503a723a90
EricLBuehler <ericlbuehler@gmail.com>
2023-06-05 07:48:02 -0400

    Use todo comment

--- src/objects/dictobject.rs
+++ src/objects/dictobject.rs
@@ -67,7 +67,7 @@ fn dict_get<'a>(selfv: Object<'a>, other: Object<'a>) -> MethodType<'a> {
 
 #[inline]
 fn dict_set<'a>(selfv: Object<'a>, other: Object<'a>, value: Object<'a>) -> MethodType<'a> {
-    //DEBUG check for hash here!
+    //TODO check for hash here!
     let mut map = selfv
         .internals
         .get_map()
